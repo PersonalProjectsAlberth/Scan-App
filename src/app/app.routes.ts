0499805996133ee2157766661cf1726376171af4
ALFAROSO <alberth.sotelo@gmail.com>
2025-04-02 11:54:21 -0700

    implement route for puzzle-car component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PuzzleCardComponent } from './puzzle-card/puzzle-card.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: PuzzleCardComponent},
+    {path: '**', redirectTo: '', pathMatch: 'full' }
+];
