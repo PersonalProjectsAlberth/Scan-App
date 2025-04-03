fbbc5d8e031eb48cdea9f7d1cbbdb230f6222c65
ALFAROSO <alberth.sotelo@gmail.com>
2025-04-03 15:08:16 -0700

    scan-shape component implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { PuzzleCardComponent } from './puzzle-card/puzzle-card.component';
 import { ShapeCardComponent } from './shape-card/shape-card.component';
+import { ScanShapeComponent } from './scan-shape/scan-shape.component';
 
 export const routes: Routes = [
     {path: '', component: PuzzleCardComponent},
     {path: 'shape', component: ShapeCardComponent},
+    {path: 'shape/:id', component: ScanShapeComponent},
+
     {path: '**', redirectTo: '', pathMatch: 'full'},
 ];
